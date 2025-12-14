5f0c86aa3dc9438ff7b1ccd4ae81ff834f638990
Liviu <68596312+liviu-b@users.noreply.github.com>
2025-12-14 20:49:15 +0000

    //

--- backend/server.py
+++ backend/server.py
@@ -2181,9 +2181,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["http://localhost:3000"],
-    allow_origins=origins,        
-    allow_credentials=True,
+    allow_origins=["http://localhost:3000"],     
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allow_headers=["*"],
     expose_headers=["*"],
